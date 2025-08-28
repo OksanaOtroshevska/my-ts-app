e809e64ed16004a011480319f8687d24a43197ff
OksanaOtroshevska <otroshevskaya@gmail.com>
2025-08-28 18:04:19 +0200

    The basket of goods has been improved, the "Delete" button has been added.

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,43 @@ import './style.css'
 
 const addToCartButtons = document.querySelectorAll<HTMLButtonElement>('.add-to-cart');
 
-const cartCountElement = document.getElementById('cart-count') as HTMLSpanElement;
+const removeFromCartButtons = document.querySelectorAll<HTMLButtonElement>('.remove-from-cart');
+
+const cartCountElement = document.querySelector<HTMLSpanElement>('#cart-count');
 
 let cartCount = Number(localStorage.getItem('cartCount')) || 0;
 
-cartCountElement.textContent = cartCount.toString();
 
+if (cartCountElement) {
+  cartCountElement.textContent = cartCount.toString();
+}
+
+// Добавление товара
 addToCartButtons.forEach(button => {
   button.addEventListener('click', () => {
     cartCount++;
-    cartCountElement.textContent = cartCount.toString();
 
+    if (cartCountElement) {
+  cartCountElement.textContent = cartCount.toString();
+}
     localStorage.setItem('cartCount', cartCount.toString());
   });
 });
+
+// Удаление товара
+removeFromCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (cartCount > 0) {   // чтобы не уходило в минус
+      cartCount--;
+      updateCartCount();
+    }
+  });
+});
+
+// Функция обновления счётчика
+function updateCartCount() {
+  if (cartCountElement) {
+  cartCountElement.textContent = cartCount.toString();
+  }
+  localStorage.setItem('cartCount', cartCount.toString());
+}
